694903a21c5de4d6f1749b5583f7c26aac2c8339
dls197 <71739994+dls197@users.noreply.github.com>
2020-10-23 19:59:59 -0400

    Add files via upload

--- Algorithm.java
+++ Algorithm.java
@@ -103,10 +103,9 @@ public class Algorithm {
 		long clock1et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock1.getNumFault());
 		System.out.println("Runtime is " + (clock1et - clock1st) + " nanoseconds");
-		System.out.println("\n");
 
 
-		System.out.println("The reference for an exponential distribution is: ");
+		System.out.println("\nThe reference for an exponential distribution is: ");
 		for (int i = 0; i < length; i++) {
 			pageRef[i] = Integer.toString(RandomGenerator.expRandom(length));
 			System.out.print(pageRef[i] + " ");
@@ -132,10 +131,9 @@ public class Algorithm {
 		long clock2et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock2.getNumFault());
 		System.out.println("Runtime is " + (clock2et - clock2st) + " nanoseconds");
-		System.out.println("\n");
 
 
-		System.out.println("The reference for a strongly biased probability to request any of 3<k<10 is: ");
+		System.out.println("\nThe reference for a strongly biased probability to request any of 3<k<10 is: ");
 		for (int i = 0; i < length; i++) {
 			pageRef[i] = Integer.toString(RandomGenerator.myRandom(length));
 			System.out.print(pageRef[i] + " ");
@@ -161,7 +159,29 @@ public class Algorithm {
 		long clock3et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock3.getNumFault());
 		System.out.println("Runtime is " + (clock3et - clock3st) + " nanoseconds");
-		System.out.println("\n");
-		
+
+		System.out.println("\nResult comparisons:");
+		System.out.println("\nequiprobable clock and equiprobable fifo faults: " + clock1.getNumFault()/fifo1.getNumFault());
+		System.out.println("equiprobable clock and equiprobable fifo runtime: " + (clock1et - clock1st)/(fifo1et - fifo1st));
+		System.out.println("e\nxponential clock and exponential fifo faults: "+ clock2.getNumFault()/fifo2.getNumFault());
+		System.out.println("exponential clock and exponential fifo runtime: " + (clock2et - clock2st)/(fifo2et - fifo2st));
+		System.out.println("\nbiased clock and biased fifo faults: " + clock3.getNumFault()/fifo3.getNumFault());
+		System.out.println("biased clock and biased fifo runtime: " + (clock3et - clock3st)/(fifo3et - fifo3st));
+
+		System.out.println("\nbiased clock and exponential clock faults: " + clock3.getNumFault()/clock2.getNumFault());
+		System.out.println("exponential clock and equiprobable clock faults: " + clock2.getNumFault()/clock1.getNumFault());
+		System.out.println("biased clock and equiprobable clock faults: " + clock3.getNumFault()/clock1.getNumFault());
+
+		System.out.println("\nbiased fifo and exponential fifo faults: " + fifo3.getNumFault()/fifo2.getNumFault());
+		System.out.println("exponential fifo and equiprobable fifo faults: " + fifo2.getNumFault()/fifo1.getNumFault());
+		System.out.println("biased fifo and equiprobable fifo faults: " + fifo3.getNumFault()/fifo1.getNumFault());
+
+		System.out.println("\nbiased clock and exponential clock runtime: " + (clock3et - clock3st)/(clock2et - clock2st));
+		System.out.println("exponential clock and equiprobable clock runtime: " + (clock2et - clock2st)/(clock1et - clock1st));
+		System.out.println("biased clock and equiprobable clock runtime: " + (clock3et - clock3st)/(clock1et - clock1st));
+
+		System.out.println("\nbiased fifo and exponential fifo runtime: " + (fifo3et - fifo3st)/(fifo2et - fifo2st));
+		System.out.println("exponential fifo and equiprobable fifo runtime: " + (fifo2et - fifo2st)/(fifo1et - fifo1st));
+		System.out.println("biased fifo and equiprobable fifo runtime: " + (fifo3et - fifo3st)/(fifo1et - fifo1st));
 	}
 }
