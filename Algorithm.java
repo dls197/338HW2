7771270930c8bb57001fccd945eaf649f23a8264
dls197 <71739994+dls197@users.noreply.github.com>
2020-10-23 19:38:59 -0400

    time tracking
    need ratios

--- Algorithm.java
+++ Algorithm.java
@@ -84,19 +84,27 @@ public class Algorithm {
 		}
 		System.out.println("\n\nFIFO algorithm:");
 		FIFO fifo1 = new FIFO(3);
+		long fifo1st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			fifo1.addPage(pageRef[i]);
 			fifo1.printFrame();
 		}
+		long fifo1et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + fifo1.getNumFault());
+		System.out.println("Runtime is " + (fifo1et - fifo1st) + " nanoseconds");
+
 		System.out.println("\nCLOCK algorithm:");
 		Clock clock1 = new Clock(3);
+		long clock1st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			clock1.addPage(pageRef[i]);
 			clock1.printClock();
 		}
+		long clock1et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock1.getNumFault());
+		System.out.println("Runtime is " + (clock1et - clock1st) + " nanoseconds");
 		System.out.println("\n");
+
 
 		System.out.println("The reference for an exponential distribution is: ");
 		for (int i = 0; i < length; i++) {
@@ -105,19 +113,27 @@ public class Algorithm {
 		}
 		System.out.println("\n\nFIFO algorithm:");
 		FIFO fifo2 = new FIFO(3);
+		long fifo2st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			fifo2.addPage(pageRef[i]);
 			fifo2.printFrame();
 		}
+		long fifo2et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + fifo2.getNumFault());
+		System.out.println("Runtime is " + (fifo2et - fifo2st) + " nanoseconds");
+
 		System.out.println("\nCLOCK algorithm:");
 		Clock clock2 = new Clock(3);
+		long clock2st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			clock2.addPage(pageRef[i]);
 			clock2.printClock();
 		}
+		long clock2et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock2.getNumFault());
+		System.out.println("Runtime is " + (clock2et - clock2st) + " nanoseconds");
 		System.out.println("\n");
+
 
 		System.out.println("The reference for a strongly biased probability to request any of 3<k<10 is: ");
 		for (int i = 0; i < length; i++) {
@@ -126,19 +142,26 @@ public class Algorithm {
 		}
 		System.out.println("\n\nFIFO algorithm:");
 		FIFO fifo3 = new FIFO(3);
+		long fifo3st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			fifo3.addPage(pageRef[i]);
 			fifo3.printFrame();
 		}
+		long fifo3et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + fifo3.getNumFault());
+		System.out.println("Runtime is " + (fifo3et - fifo3st) + " nanoseconds");
+
 		System.out.println("\nCLOCK algorithm:");
 		Clock clock3 = new Clock(3);
+		long clock3st = System.nanoTime();
 		for (int i = 0; i < length; i++) {
 			clock3.addPage(pageRef[i]);
 			clock3.printClock();
 		}
+		long clock3et = System.nanoTime();
 		System.out.println("Number of Page Fault Occurs: " + clock3.getNumFault());
+		System.out.println("Runtime is " + (clock3et - clock3st) + " nanoseconds");
 		System.out.println("\n");
-
+		
 	}
 }
